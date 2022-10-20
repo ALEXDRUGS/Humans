633ecfba05b6b1418ec8673a7f820956ddf37e50
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-10-20 13:03:16 +0300

    ДЗ human2

--- src/Human.java
+++ src/Human.java
@@ -3,21 +3,28 @@ public class Human {
     private final String name;
     private final String town;
 
+    private final String job;
+
     public int getYearOfBirth() {
         return yearOfBirth;
     }
 
     public String getName() {
-        return (String) name;
+        return name;
     }
 
     public String getTown() {
         return town;
     }
 
-    public Human(String name,int yearOfBirth, String town) {
+    public String getJob() {
+        return job;
+    }
+
+    public Human(String name,int yearOfBirth, String town, String job) {
         this.yearOfBirth = yearOfBirth;
         this.name = name;
         this.town = town;
+        this.job = job;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,15 @@ public class Main {
         return current_date.getYear();
     }
     public static void printHuman(Human human){
-        System.out.println("Привет! Меня зовут: " + human.getName() + ". Мой год рождения: "
-                + (currentYear() - human.getYearOfBirth()) + "й. Я живу в городе: " + human.getTown());
+        System.out.println("Привет! Меня зовут " + human.getName() + ". Мой год рождения "
+                + (currentYear() - human.getYearOfBirth()) + "й. Я живу в городе " + human.getTown()
+                + ". Я работаю на должности " + human.getJob() + ". Будем знакомы!");
     }
     public static void main(String[] args) {
-        Human human = new Human( "Максим", 35,"Минск");
-        Human human1 = new Human( "Аня", 29,"Москва");
-        Human human2 = new Human( "Катя", 28,"Калининград");
-        Human human3 = new Human( "Артём", 27,"Москва");
+        Human human = new Human( "Максим", 35,"Минск", "бренд-менеджер");
+        Human human1 = new Human( "Аня", 29,"Москва", "методист образовательных программ");
+        Human human2 = new Human( "Катя", 28,"Калининград", "продакт-менеджер");
+        Human human3 = new Human( "Артём", 27,"Москва", "директор по развитию бизнеса");
         printHuman(human);
         printHuman(human1);
         printHuman(human2);
